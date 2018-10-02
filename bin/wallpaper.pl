779ddc7badbddd1cd055e531d4a75ccee8dc1009
zach <zao@San.my.domain>
2018-10-01 19:54:13 -0600

    Add perl version of wallpaper script

---
+++ bin/wallpaper.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+# Wallpaper manager (in perl)
+# Chooses random backgrounds on startup.
+# Sending this script SIGUSR1 will change wallpaper.
+# Has a home and not home setting for more...fun papers ;^)
+
+use strict;
+use warnings;
+
+# Capture Signal SIGUSR1
+$SIG{USR1} = \&handleSig;
+
+# Globals
+my $HOMEWIFI = ""; # "CenturyLink0014";
+my $DIR = ".w";
+my $TIMEOUT = 5;
+my $LOC = 1;
+my $PAPER = "";
+
+sub getwall {
+#print "getwall\n";
+  my @PAPES = glob( "~/Pictures/.w/*" );
+  $PAPER = $PAPES[int(rand(scalar(@PAPES)))];
+}
+
+sub handleSig {
+  getwall();
+#print "SIGNAL\n";
+}
+
+sub checkwifi {
+  my $SSID = `ifconfig` =~ /nwid (.*) chan/;
+
+  if ( $SSID = $HOMEWIFI ){
+    $DIR = ".nsw";
+    $TIMEOUT = 5;
+    $LOC = 1;
+  } else {
+    $DIR= ".w" ;
+    $TIMEOUT = 60;
+    $LOC = 2;
+  }
+}
+
+
+sub setwall {
+#print "setwall\n";
+  my $OLDLOC = $LOC;
+  system( "feh", "$PAPER", "--bg-fill" );
+
+  checkwifi();
+  if ( $OLDLOC != $LOC ) {
+    getwall();
+  }
+}
+
+# killall -q wallpaper.sh
+for my $i ( `pgrep perl` ) {
+  if ( $i != $$ ) {
+    system( "kill -9 $i" );
+  }
+}
+
+checkwifi();
+getwall();
+
+while ( 1 ) {
+  setwall();
+  sleep( $TIMEOUT );
+}
+
